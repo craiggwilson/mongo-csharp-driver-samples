6bf0a371a993b909d66ada6b561d6b4caf900954
Craig Wilson <craiggwilson@gmail.com>
2014-04-07 14:57:45 -0500

    moved over to a web project (still using Nancy).

--- src/Milieu/Milieu.Web2/Properties/AssemblyInfo.cs
+++ src/Milieu/Milieu.Web2/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("31af9534-90e8-448f-a2e4-0b3054df59e3")]
+[assembly: Guid("04e96487-57c9-44ef-bcb5-ee905a7d4d6e")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
