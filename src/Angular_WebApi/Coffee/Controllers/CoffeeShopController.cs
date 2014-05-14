dbb898144d0b818fd8176778df0243542094df61
Craig Wilson <craiggwilson@gmail.com>
2014-05-14 12:39:24 -0500

    prepped for introducing Mongo.

---
+++ src/Angular_WebApi/Coffee/Controllers/CoffeeShopController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Coffee.Models;
+
+namespace Coffee.Controllers
+{
+    [RoutePrefix("api/coffeeshop")]
+    public class CoffeeShopController : ApiController
+    {
+        [Route("{id:int}/order")]
+        [HttpPost]
+        public HttpResponseMessage Order(int id, [FromBody] DrinkOrder drinker)
+        {
+            return Request.CreateErrorResponse(HttpStatusCode.NotImplemented, "not yet");
+        }
+    }
+}
---
+++ src/Angular_WebApi/Coffee/Models/DrinkOrder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Coffee.Models
+{
+    public class DrinkOrder
+    {
+        public DrinkType Type { get; set; }
+
+        public string Size { get; set; }
+
+        public string Drinker { get; set; }
+    }
+}
